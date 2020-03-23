e366bcfff16c1768ea464d1b91d5793c081f2339
James Waterhouse <james.waterhouse@yahoo.co.uk>
2020-03-23 13:59:51 +0000

    first commit. adding php code to get database connection

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+
+    $db = new PDO('mysql:host=db; dbname=birds', 'root', 'password');
+
+$query = $db->prepare("SELECT `species`, `color`, `frequency`, `shit` FROM `birds`;");
+
+$query->setFetchMode(PDO::FETCH_ASSOC);
+
+$query->execute();
+
+$result = $query->fetchAll();
+
+echo $result;
+
+
+?>
