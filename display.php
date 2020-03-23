eba21ca29e3207a41d58cb8b766703c2b8c70676
James Waterhouse <james.waterhouse@yahoo.co.uk>
2020-03-23 16:23:01 +0000

    styles

---
+++ display.php
@@ -0,0 +1,22 @@
+<?php 
+
+require "database.php";
+
+function display($array) {
+
+    foreach ($array as $bird) {
+            echo '<div class="item">
+                    <div class="pic">
+                        <img src= "'; echo $bird['picture']; echo'">
+                    </div>';
+            echo '<div class="text">
+                    <ul class="attributes">';
+                    echo '<li>Species: '; echo $bird["species"]; echo'</li>';
+                    echo '<li>Colour: '; echo $bird["color"]; echo'</li>';
+                    echo '<li>Frequency: '; echo $bird["frequency"]; echo'</li>';
+                    echo '<li>Has it shit on me? '; echo $bird["shit"]; echo'</li>';
+                echo '</ul>
+                </div>  
+            </div>';
+        }
+    }
--- index.php
+++ index.php
@@ -1,55 +1,19 @@
 <!doctype html>
-
 <html lang='en'>
-<head>
-
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <title>birds and that</title>
-    <?php
-    require "database.php";
-    ?>
-    
-    
-
-</head>
-
-<body>
-    <header>
-        <h1>
-        What birds are outside my window?
-        </h1>
-    </header>
-
-    <section >
-
-    
-    <?php foreach($result as $bird) {
-        echo '<div class="item">
-            <div class="pic">
-                <img src= "';
-                
-                echo $bird['picture'];
-                
-                echo'">
-            </div>';
-
-
-            echo '<div class="text">
-                <ul class="attributes">';
-                   echo '<li>Species: '; echo $bird["species"]; echo'</li>';
-                   echo '<li>Colour: '; echo $bird["color"]; echo'</li>';
-                   echo '<li>Frequency: '; echo $bird["frequency"]; echo'</li>';
-                   echo '<li>Has it shit on me? '; echo $bird["shit"]; echo'</li>';
-               echo '</ul>
-            </div>  
-        </div>';
-    }
-
-?>
-
-
-    </section>
-</body>
-
+    <head>
+        <link href="styles/normalize.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <title>birds and that</title>
+        <?php require "display.php";?>
+    </head>
+    <body>
+        <header>
+            <h1>What birds are outside my window?</h1>
+        </header>
+        <section >
+            <?php display($result);?>
+        </section>
+    </body>
 </html>
 
